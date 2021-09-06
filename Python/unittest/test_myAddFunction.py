f1c7b7e0a05bbc739e8a7d21349cab013242759a
Rudi Niemeijer <rudi.niemeijer@kpn.com>
2021-09-06 19:52:30 +0200

    testing

--- Python/unittest/test_myAddFunction.py
+++ Python/unittest/test_myAddFunction.py
@@ -1,6 +1,6 @@
 from myCode import myAddFunction # This is the function we want to test
 import unittest # The Python Unittest framework
 
-class Test(unittest.TestCase): # Subclassing the TestCase class
-  def test_sum_zeroes(self): # A test, that has to start with 'test_'
-    self.assertEquals(myAddFunction(0,0), 0)
+class Test(unittest.TestCase):    # Subclassing the TestCase class
+  def test_sum_zeroes(self):      # A test, that has to start with 'test_'
+    self.assertEqual(myAddFunction(0,0), 0)
